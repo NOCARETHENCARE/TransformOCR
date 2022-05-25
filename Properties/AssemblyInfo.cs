799c9dcd2b0e808d805e487fdf7f0d595f75dd3a
#mooddream <73425839+NOCARETHENCARE@users.noreply.github.com>
2022-05-25 10:10:12 +0900

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCopyright(" Microsoft 2019")]
+[assembly: AssemblyProduct("TransformOCR")]
+[assembly: AssemblyCompany("Github")]
+[assembly: Guid("b42ca6f6-3a1a-44b5-9993-eab600012397")]
+[assembly: AssemblyTitle("TransformOCR")]
+[assembly: AssemblyDescription("SpongeBob SquarePants")]
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: CLSCompliant(true)]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
